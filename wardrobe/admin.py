976fd3c382c5fc3f8472fb318ecc76f04801f7dd
davykanye <74606340+davykanye@users.noreply.github.com>
2021-04-30 22:49:20 +0100

    Add files via upload

---
+++ wardrobe/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Photos, Category, Outfit
+
+
+class OutfitAdmin(admin.ModelAdmin):
+    change_form_template = 'admin/outfitview.html'
+
+class PhotosAdmin(admin.ModelAdmin):
+    list_display = ['admin_photo','description', 'category']
+
+
+admin.site.register(Category)
+admin.site.register(Photos, PhotosAdmin)
+admin.site.register(Outfit, OutfitAdmin)
+
+# admin.site.register(Pizza)
+# admin.site.register(Topping)
